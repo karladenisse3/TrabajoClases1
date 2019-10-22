c197691a9786633b01f7ef2668265da2b8a784d7
karladenisse3 <56401744+karladenisse3@users.noreply.github.com>
2019-10-21 21:26:12 -0500

    Add files via upload

---
+++ Prestamo.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EJERCICIO1.ClaseEncapsulamiento
+{
+    public class Prestamo
+    {
+        public Prestamo()
+        {
+
+        }
+
+        private string cancha;
+
+        public string Cancha
+        {
+            get { return cancha; }
+            set { cancha = value; }
+        }
+
+
+        public float Costo
+        {
+            get {
+                if (this.Cancha=="FUTBOL")
+                  {
+                      return 20;
+                  }
+                  else if (this.Cancha=="FULBITO")
+                  {
+                      return 15;
+                  }
+                  else
+                  {
+                      return 5;
+                  }
+
+            }
+        }
+
+        private int horas;
+
+        public int Horas
+        {
+            get { return horas; }
+            set {
+                if (value > 3)
+                {
+                    throw new Exception("No se puede solicitar ninguna cancha por más de 3 horas.");//Para crear una exception
+                }
+
+                horas = value; }
+        }
+
+
+        public double Subtotal
+        {
+            get {
+                return Costo*Horas;
+            }
+           
+        }
+        private double descuento;
+
+        public double Descuento
+        {
+            get {
+             
+                return descuento;
+            }
+            set
+            {
+                if (value!=10 && value!=20 )
+                {
+                    throw new Exception("El descuento debe ser entre 10% y 20% ");
+
+                }
+                descuento = value;
+
+            }
+
+        }
+
+      
+        public double Subtotal2
+        {
+            get {
+
+                return Subtotal - (Subtotal*Descuento/100);
+            }
+        }
+
+   
+        public double Iva
+        {
+            get { return Subtotal2*0.12; }
+        }
+
+   
+        public double Final
+        {
+            get {
+                return Subtotal2+Iva;
+            }
+        }
+
+        private double pagado;
+
+        public double Pagado
+        {
+            get { return pagado; }
+            set {
+                if (value < (Subtotal*0.5) )
+                {
+                    throw new Exception("Debe cancelar por lo menos el 50% de la cancha");//Para crear una exception
+                }
+                pagado = value;
+            }
+        }
+
+    
+        public double Pendiente
+        {
+            get { return Final-Pagado; }
+        }
+
+        private string estado;
+
+        public string Estado
+        {
+            get {
+                if (Pendiente == 0)
+                {
+                    Console.WriteLine("PAGADO");
+                }
+                else
+                {
+                    Console.WriteLine("PENDIENTE");
+                }
+                return estado;
+            }
+            set { estado = value; }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using EJERCICIO1.ClaseEncapsulamiento;
+
+namespace EJERCICIO1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Int16 x = Con Console.ReadLine();
+
+            Prestamo prestamo=
+                  new Prestamo();
+            Console.WriteLine("Ingrese cancha a utilizar");
+            String can = Console.ReadLine();
+            prestamo.Cancha=can;
+
+            Console.WriteLine("Ingrese horas a utilizar");
+            string linea = Console.ReadLine();
+            int hor = int.Parse(linea);
+            prestamo.Horas = hor;
+            
+            Console.WriteLine("Ingrese el anticipo o pago a cancelar");
+            double pag = double.Parse(Console.ReadLine());
+            prestamo.Pagado = pag;
+
+            Console.WriteLine("Ingrese el descuento a aplicar que es entre 10% y 20%");
+            double des = double.Parse(Console.ReadLine());
+            prestamo.Descuento = des;
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Subtotal: " + prestamo.Subtotal);
+            Console.WriteLine("Descuento: " + prestamo.Descuento);
+            Console.WriteLine("IVA: " + prestamo.Iva);
+            Console.WriteLine("Total Final: " + prestamo.Final);
+            Console.WriteLine("Valor Pagado: " + prestamo.Pagado);
+            Console.WriteLine("Saldo Pendiente: " + prestamo.Pendiente);
+            Console.WriteLine("Deuda: " + prestamo.Estado);
+            Console.Read();
+        }
+    }
+}
